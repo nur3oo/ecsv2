195df718ac2c69bad44a4ef61a754a6546d23bd5
nur3oo <dnurdaud@gmail.com>
2026-03-29 21:56:09 +0100

    added redis

---
+++ infra/redis/main.tf
@@ -0,0 +1,34 @@
+resource "aws_elasticache_replication_group" "this" {
+  replication_group_id = "${var.name}-redis"
+  description = "Redis"
+
+  engine         = "redis"
+  engine_version = var.engine_version
+  node_type      = var.node_type
+  port           = var.port
+
+  subnet_group_name  = aws_elasticache_subnet_group.this.name
+  security_group_ids = [aws_security_group.redis.id]
+
+  num_cache_clusters         = var.num_cache_clusters
+  multi_az_enabled           = var.multi_az_enabled
+  automatic_failover_enabled = var.automatic_failover_enabled
+
+
+  at_rest_encryption_enabled = true
+  transit_encryption_enabled = false
+
+  tags = {
+    Name = "${var.name}-redis"
+  }
+}
+
+
+resource "aws_elasticache_subnet_group" "this" {
+  name       = "${var.name}-redis-subnets"
+  subnet_ids = var.private_subnet_ids
+
+  tags = {
+    Name = "${var.name}-redis-subnets"
+  }
+}
---
+++ infra/redis/variables.tf
@@ -0,0 +1,42 @@
+variable "engine_version" {
+  type        = string
+  description = "Redis engine "
+  default     = "7.0"
+}
+
+variable "port" {
+  type    = number
+  default = 6379
+}
+
+variable "multi_az_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "automatic_failover_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "num_cache_clusters" {
+  type        = number
+  description = "Number of cache nodes (1 for dev). If you enable failover, set to 2+."
+  default     = 1
+}
+
+variable "node_type" {
+  type        = string
+  description = "elasticache node type(e.g. cache.t4g.micro)"
+  default     = "cache.t4g.micro"
+}
+
+variable "name" {
+  type        = string
+  description = "Name prefix for redis resources"
+}
+
+variable "private_subnet_ids" {
+    type = list(string)
+  
+}
