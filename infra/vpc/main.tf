38be54a296839556ec320c2354943f70feca5bf6
nur3oo <dnurdaud@gmail.com>
2026-04-06 19:45:58 +0100

    fixed a typo with the vpc

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id = var.vpc_id
+  vpc_id = aws_vpc.main.id
   count = var.count
   cidr_block = var.public_subnet_cidrs[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
@@ -20,15 +20,15 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  vpc_id = var.vpc_id
+  vpc_id = aws_vpc.main.id
   count = var.count
   cidr_block = var.private_subnet_cidrs[count.index]
-  availability_zone = data.aws_availability_zones.available
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = var.vpc_id
+  vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
@@ -38,12 +38,12 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public" {
   count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[*].id
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table" "private" {
-  vpc_id = var.vpc_id
+  vpc_id = aws_vpc.main.id
 }
 
 resource "aws_route_table_association" "private" {
@@ -54,13 +54,13 @@ resource "aws_route_table_association" "private" {
 
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = var.vpc_id
+  vpc_id = aws_vpc.main.id
   
 }
 
 
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id = var.vpc_id
+  vpc_id = aws_vpc.main.id
   vpc_endpoint_type = "Gateway"
   route_table_ids = [aws_route_table.private.id]
   service_name = "com.amazonaws.${var.region}.s3"
