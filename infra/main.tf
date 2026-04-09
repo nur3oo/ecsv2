eacb3e44e1725cac8ac29ab9dcc52ea58bd49148
nur3oo <dnurdaud@gmail.com>
2026-04-09 12:37:39 +0100

    extra = in main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,7 @@ module "ecs" {
 
 module "alb" {
     source = "./alb"
-    vpc_id =  = module.vpc.vpc_id
+    vpc_id =   module.vpc.vpc_id
     public_subnets = module.vpc.public_subnets
     acm_cert = module.acm.acm_cert
     alb_sg = module.sg.alb_sg
